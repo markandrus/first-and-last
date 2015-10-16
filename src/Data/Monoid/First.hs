86443af6bc47561ae7fb68290c9eec80ed9df8cf
Mark Andrus Roberts <mroberts@twilio.com>
2015-10-15 18:36:46 -0700

    Reimplement in terms of DList

--- src/Data/Monoid/First.hs
+++ src/Data/Monoid/First.hs
@@ -39,14 +39,16 @@ module Data.Monoid.First
   , getFirst'
   ) where
 
-import Control.Applicative (Applicative((<*>), pure), Alternative)
-import Data.Data (Data)
+import Control.Applicative (Applicative((<*>), pure), Alternative((<|>), empty))
+import Control.DeepSeq (NFData)
+import Data.DList (DList)
+import qualified Data.DList as DList
 import Data.Proxy (Proxy(Proxy))
 import Data.String (IsString(fromString))
 import GHC.Exts (IsList(Item, fromList, toList))
 import GHC.Generics (Generic, Generic1)
 import GHC.TypeLits (KnownNat, Nat, natVal)
-import Prelude (($), (.), Char, Eq((==)), Foldable(foldr), Functor, Maybe(Just, Nothing), Monoid(mappend, mempty), Ord, Read, Show, Traversable, fromIntegral, head, take)
+import Prelude (($), (.), Char, Eq((==)), Foldable(foldr), Functor(fmap), Int, Maybe(Just, Nothing), Monoid(mappend, mempty), Ord, Read, Show, Traversable(sequenceA), fromIntegral, head, take)
 
 -- $setup
 -- >>> import Prelude
@@ -74,18 +76,16 @@ getFirst (getFirst' -> as) = Just . head $ take 1 as
 --------------------------------------------------------------------------------
 
 -- | A generalized version of @Data.Monoid.First@
-newtype First' (n :: Nat) a = First' { _getFirst' :: [a] } deriving 
-  ( Alternative
-  , Data
-  , Eq
+newtype First' (n :: Nat) a = First' { _getFirst' :: (Int, DList a) } deriving
+  ( Eq
   , Foldable
   , Functor
   , Generic
   , Generic1
+  , NFData
   , Ord
   , Read
   , Show
-  , Traversable
   )
 
 -- | Get the first @n@ values or fewer of type @a@.
@@ -99,19 +99,23 @@ newtype First' (n :: Nat) a = First' { _getFirst' :: [a] } deriving
 -- >>> getFirst' (foldMap pure [1,2,3,4] :: First' 2 Int)
 -- [1,2]
 getFirst' :: First' n a -> [a]
-getFirst' = _getFirst'
+getFirst' (First' (n, as)) = take n $ DList.toList as
+
+instance KnownNat n => Alternative (First' n) where
+  First' (n, l) <|> First' (_, r) = First' (n, l <|> r)
+  empty = mempty
 
 instance KnownNat n => Applicative (First' n) where
-  First' l <*> First' r = First' $ l <*> r
+  First' (n, l) <*> First' (_, r) = First' (n, l <*> r)
   pure a =
     let n = natVal (Proxy :: Proxy n)
-    in  First' $ if n == 0 then mempty else pure a
+    in  First' (fromIntegral n, if n == 0 then mempty else pure a)
 
 instance KnownNat n => Monoid (First' n a) where
-  First' l `mappend` First' r =
-    let n = fromIntegral $ natVal (Proxy :: Proxy n)
-    in  First' . take n $ l `mappend` r
-  mempty = First' mempty
+  First' (n, l) `mappend` First' (_, r) = First' (n, l `mappend` r)
+  mempty =
+    let n = natVal (Proxy :: Proxy n)
+    in  First' (fromIntegral n, mempty)
 
 instance KnownNat n => IsList (First' n a) where
   type Item (First' n a) = a
@@ -120,3 +124,6 @@ instance KnownNat n => IsList (First' n a) where
 
 instance KnownNat n => IsString (First' n Char) where
   fromString = fromList
+
+instance Traversable (First' n) where
+  sequenceA (First' (n, as)) = fmap (\as' -> First' (n, fromList as')) . sequenceA $ toList as
--- src/Data/Monoid/Last.hs
+++ src/Data/Monoid/Last.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE KindSignatures #-}
@@ -39,14 +40,16 @@ module Data.Monoid.Last
   , getLast'
   ) where
 
-import Control.Applicative (Applicative((<*>), pure), Alternative)
-import Data.Data (Data)
+import Control.Applicative (Applicative((<*>), pure), Alternative((<|>), empty))
+import Control.DeepSeq (NFData)
+import Data.DList (DList)
+import qualified Data.DList as DList
 import Data.Proxy (Proxy(Proxy))
 import Data.String (IsString(fromString))
 import GHC.Exts (IsList(Item, fromList, toList))
 import GHC.Generics (Generic, Generic1)
 import GHC.TypeLits (KnownNat, Nat, natVal)
-import Prelude (($), (.), Char, Eq((==)), Foldable(foldr), Functor, Int, Maybe(Just, Nothing), Monoid(mappend, mempty), Ord, Read, Show, Traversable, fromIntegral, drop, error, head)
+import Prelude (($), (.), Char, Eq((==)), Foldable(foldr), Functor(fmap), Int, Maybe(Just, Nothing), Monoid(mappend, mempty), Ord, Read, Show, Traversable(sequenceA), fromIntegral, drop, error, head)
 
 -- $setup
 -- >>> import Prelude
@@ -74,18 +77,16 @@ getLast (getLast' -> as) = Just . head $ takeR 1 as
 --------------------------------------------------------------------------------
 
 -- | A generalized version of @Data.Monoid.Last@
-newtype Last' (n :: Nat) a = Last' { _getLast' :: [a] } deriving
-  ( Alternative
-  , Data
-  , Eq
+newtype Last' (n :: Nat) a = Last' { _getLast' :: (Int, DList a) } deriving
+  ( Eq
   , Foldable
   , Functor
   , Generic
   , Generic1
+  , NFData
   , Ord
   , Read
   , Show
-  , Traversable
   )
 
 -- | Get the last @n@ values or fewer of type @a@.
@@ -99,19 +100,23 @@ newtype Last' (n :: Nat) a = Last' { _getLast' :: [a] } deriving
 -- >>> getLast' (foldMap pure [1,2,3,4] :: Last' 2 Int)
 -- [3,4]
 getLast' :: Last' n a -> [a]
-getLast' = _getLast'
+getLast' (Last' (n, as)) = takeR n $ DList.toList as
+
+instance KnownNat n => Alternative (Last' n) where
+  Last' (n, l) <|> Last' (_, r) = Last' (n, l <|> r)
+  empty = mempty
 
 instance KnownNat n => Applicative (Last' n) where
-  Last' l <*> Last' r = Last' $ l <*> r
+  Last' (n, l) <*> Last' (_, r) = Last' (n, l <*> r)
   pure a =
     let n = natVal (Proxy :: Proxy n)
-    in  Last' $ if n == 0 then mempty else pure a
+    in  Last' (fromIntegral n, if n == 0 then mempty else pure a)
 
 instance KnownNat n => Monoid (Last' n a) where
-  Last' l `mappend` Last' r =
-    let n = fromIntegral $ natVal (Proxy :: Proxy n)
-    in  Last' . takeR n $ l `mappend` r
-  mempty = Last' mempty
+  Last' (n, l) `mappend` Last' (_, r) = Last' (n, l `mappend` r)
+  mempty =
+    let n = natVal (Proxy :: Proxy n)
+    in  Last' (fromIntegral n, mempty)
 
 -- https://www.joachim-breitner.de/blog/600-On_taking_the_last_n_elements_of_a_list
 takeR :: Int -> [a] -> [a]
@@ -128,3 +133,6 @@ instance KnownNat n => IsList (Last' n a) where
 
 instance KnownNat n => IsString (Last' n Char) where
   fromString = fromList
+
+instance Traversable (Last' n) where
+  sequenceA (Last' (n, as)) = fmap (\as' -> Last' (n, fromList as')) . sequenceA $ toList as
